8705da58c08bb4ecb76bcd2e2ba4668db7c1bcfb
sudeis <sudeisfed@gmail.com>
2024-09-05 03:30:47 +0300

    sdfg

--- server/app.js
+++ server/app.js
@@ -48,7 +48,10 @@ app.use('/',(req,res)=>{
     res.send("hello world")
 })
 
-
+app.use((req, res, next) => {
+    console.log(`Request received: ${req.method} ${req.url}`);
+    next();
+  });
 
 
 app.listen(3000,()=>{
