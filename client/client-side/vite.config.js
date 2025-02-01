383e655969acc808716d555890fa2024f874ff0f
sudeisf <sudeisfed@gmail.com>
2025-02-01 21:40:18 +0300

    feat: add Tailwind CSS configuration and integrate with Vite for styling

--- client/client-side/vite.config.js
+++ client/client-side/vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
+import tailwindcss from 'tailwindcss'; // Import the new package
+import autoprefixer from 'autoprefixer';
 import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'process.env': process.env
-  }
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss(),
+        autoprefixer(),
+      ],
+    },
+  },
 });
